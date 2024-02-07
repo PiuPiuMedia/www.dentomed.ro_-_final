cf482fe65275e96e3c6dd3a397917e13ba975057
PiuPiuMedia <130344098+PiuPiuMedia@users.noreply.github.com>
2024-02-07 20:07:49 +0000

    optimizare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,12 @@
+
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+ 
+export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
