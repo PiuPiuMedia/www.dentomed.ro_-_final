9504924771b2ec3980aff8555b4e84cb9826dcb9
PiuPiuMedia <130344098+PiuPiuMedia@users.noreply.github.com>
2024-02-12 15:58:43 +0000

    ---

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import react from '@astrojs/react';
 
 export default defineConfig({
-  renderers: [react()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
