fbec5c3fec626b2d159e0d6a38c0acd7352696ed
PiuPiuMedia <130344098+PiuPiuMedia@users.noreply.github.com>
2024-02-12 14:37:55 +0000

    +++

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/static";
-import cookie from 'cookie'
-
-// https://astro.build/config
+import vercel from '@astrojs/vercel/serverless';
+ 
 export default defineConfig({
-  output: "static",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
