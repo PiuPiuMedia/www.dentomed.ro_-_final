4b40f6013b71394313401b5fb6d6eeb650499e25
PiuPiuMedia <130344098+PiuPiuMedia@users.noreply.github.com>
2024-02-14 16:34:20 +0000

    slider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   renderers: [react()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: netlify()
 });
