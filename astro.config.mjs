0abcc4411de1121ce729dd485110dec3a933eb27
PiuPiuMedia <130344098+PiuPiuMedia@users.noreply.github.com>
2024-02-17 09:39:56 +0000

    ge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
+----
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-import react from '@astrojs/react';
+----
 
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
 });
+
