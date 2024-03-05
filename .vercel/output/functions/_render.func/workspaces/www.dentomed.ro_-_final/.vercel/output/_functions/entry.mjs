28d111bc1f818da87dc56a0b425b1d14161758c8
PiuPiuMedia <130344098+PiuPiuMedia@users.noreply.github.com>
2024-03-05 12:12:10 +0000

    tina cms

--- .vercel/output/functions/_render.func/workspaces/www.dentomed.ro_-_final/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/workspaces/www.dentomed.ro_-_final/.vercel/output/_functions/entry.mjs
@@ -1,5 +1,5 @@
 import { renderers } from './renderers.mjs';
-import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_KTyA_0OZ.mjs';
+import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_hI245_oL.mjs';
 import 'node:fs';
 import { e as appendForwardSlash, j as joinPaths, t as trimSlashes, s as slash, p as prependForwardSlash, r as removeTrailingForwardSlash, f as collapseDuplicateSlashes } from './chunks/astro/assets-service_9YgOcYDi.mjs';
 import { R as ROUTE_DATA_SYMBOL, A as AstroError, k as ResponseSentError, l as MiddlewareNoDataOrNextCalled, n as MiddlewareNotAResponse, G as GetStaticPathsRequired, o as InvalidGetStaticPathsReturn, p as InvalidGetStaticPathsEntry, q as GetStaticPathsExpectedParams, t as GetStaticPathsInvalidRouteParam, P as PageNumberParamNotFound, N as NoMatchingStaticPathFound, u as PrerenderDynamicEndpointPathCollide, v as LocalsNotAnObject, w as ASTRO_VERSION, C as ClientAddressNotAvailable, S as StaticClientAddressNotAvailable, x as renderEndpoint, y as ReservedSlotName, z as renderSlotToString, B as renderJSX, D as chunkToString, F as CantRenderPage, H as renderPage$1, J as REROUTE_DIRECTIVE_HEADER } from './chunks/astro_Jbi09sDw.mjs';
@@ -2224,7 +2224,7 @@ const _page16 = () => import('./chunks/politica-cookie_OJwers3Z.mjs');
 const _page17 = () => import('./chunks/politica-de-calitate_ED7Je8PY.mjs');
 const _page18 = () => import('./chunks/politica-de-confidentialitate_RFPARuMC.mjs');
 const _page19 = () => import('./chunks/preturi_jOF-Qp-8.mjs');
-const _page20 = () => import('./chunks/programare_EeaQPWPU.mjs');
+const _page20 = () => import('./chunks/programare_hI7eXqvB.mjs');
 const _page21 = () => import('./chunks/protetica_tIGalejY.mjs');
 const _page22 = () => import('./chunks/recenzii_69yhcweU.mjs');
 const _page23 = () => import('./chunks/servicii_Ja2v8SKx.mjs');
@@ -2265,7 +2265,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "21412d44-d317-4333-9192-d641d047a7be"
+    "middlewareSecret": "4fa1103a-d449-412b-955d-19e1e8457455"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
